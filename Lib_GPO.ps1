bd46466750c5b8920f3c2aeef2aa05877c49a66c
Atlas2590 <v.dellacorte90@gmail.com>
2026-04-30 13:20:46 +0200

    fix path cartella bitlocker_key con variabile d'ambiente nome pc

--- Lib_GPO.ps1
+++ Lib_GPO.ps1
@@ -35,7 +35,7 @@ function Show-MenuGPO {
             '1' {
                 # --- CONTROLLO E CREAZIONE PERCORSO ---
                 $LocalPath = "C:\DATI\CED\MDS\BITLOCKER_KEY"
-                $NetworkPath = "\\SVRCENTRALE\CED\MDS\BITLOCKER_KEY\"
+                $NetworkPath = "\\$env:COMPUTER_NAME\CED\MDS\BITLOCKER_KEY\"
 
                 Write-Host "Verifica percorso: $LocalPath..." -ForegroundColor Yellow
                 if (!(Test-Path $LocalPath)) {
@@ -75,7 +75,7 @@ function Show-MenuGPO {
                 Pausa
             }
             '2' {
-                $WallSettings = @( @{ Name="Wallpaper"; Type="String"; Value="\\SVRCENTRALE\CONDIVISA\sfondo.jpg" } )
+                $WallSettings = @( @{ Name="Wallpaper"; Type="String"; Value="\\$NOME_PC\CONDIVISA\sfondo.jpg" } )
                 Applica-GPO-Pacchetto -GPOName "MDS-Wallpaper" -RegPath "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Settings $WallSettings
                 Pausa
             }
